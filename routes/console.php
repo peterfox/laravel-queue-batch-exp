735191b71d872c7512f370db75a8f49e5366a2c5
Peter Fox <peter.fox@peterfox.me>
2023-10-11 21:30:29 +0100

    Batch queuing experiment

--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,36 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('qt', function () {
+    \Illuminate\Support\Facades\Bus::batch([
+        function () {
+            ray()->send('first');
+        },
+        function () {
+            ray()->send('second');
+        },
+    ])
+        ->then(function (\Illuminate\Bus\Batch $batch) {
+            ray()->send($batch->finishedAt);
+
+            if ($batch->processedJobs() >= 3) {
+                return;
+            }
+
+            $batch->add([
+                function () {
+                    \Illuminate\Support\Sleep::sleep(15);
+                    ray()->send('third');
+                },
+            ]);
+
+            $batch = $batch->fresh();
+
+            ray($batch->finishedAt);
+        })
+        ->dispatch();
+
+    $this->comment('Fired');
+})
+    ->purpose('Test Batching');
